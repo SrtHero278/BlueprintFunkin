bac3ef1c43556ed47f0b5cf3c5674a0ae92c64df
SrtHero278 <srtpro278@gmail.com>
2025-05-11 20:42:39 -0700

    animated icon support

--- src/objects/HealthIcon.hx
+++ src/objects/HealthIcon.hx
@@ -3,9 +3,10 @@ package objects;
 import bindings.CppHelpers;
 import blueprint.graphics.Texture;
 
-class HealthIcon extends blueprint.objects.Sprite {
+using StringTools;
+
+class HealthIcon extends blueprint.objects.AnimatedSprite {
 	public var iconScale:Float = 1.0;
-	public var iconSteps:Int = 2;
 	public var health(default, set):Float = 1.0;
 	public var invertHealth:Bool = false;
 
@@ -19,16 +20,48 @@ class HealthIcon extends blueprint.objects.Sprite {
 		var path = Paths.image("game/icons/" + icon);
 		if (!sys.FileSystem.exists(path))
 			path = Paths.image("game/icons/UNKNOWN-ICON");
-		texture = Texture.getCachedTex(path);
+
+		final xmlPath = path.replace(".png", ".xml");
+		if (sys.FileSystem.exists(xmlPath)) {
+			loadFrames(xmlPath);
+
+			addPrefixAnim("normal", "normal", 24, true);
+			addPrefixAnim("losing", "losing", 24, true);
+			addPrefixAnim("winning", "winning", 24, true);
+
+			if (animData["losing"].indexes.length <= 0)
+				animData.remove("losing");
+			if (animData["winning"].indexes.length <= 0)
+				animData.remove("winning");
+		} else {
+			final text = Texture.getCachedTex(path);
+			
+			var iconSteps = Math.round(text.width / text.height);
+			loadTilesFromTex(text, text.width / iconSteps, text.height);
+			
+			addBasicAnim("normal", [0], 1, false);
+			if (iconSteps >= 2)
+				addBasicAnim("losing", [1], 1, false);
+			if (iconSteps >= 3)
+				addBasicAnim("winning", [2], 1, false);
+		}
 		
-		iconSteps = Math.round(texture.width / texture.height);
-		sourceRect.width = texture.width / iconSteps;
-		iconScale = 150.0 / texture.height;
+		playAnim("normal");
+		iconScale = 150.0 / animHeight;
 	}
 
 	function set_health(value:Float) {
 		var hp = invertHealth ? (1.0 - value) : value;
-		sourceRect.x = sourceRect.width * (CppHelpers.boolToInt(hp < 0.2 && iconSteps >= 2) + CppHelpers.boolToInt(hp > 0.8 && iconSteps >= 3) * 2);
+
+		var queueAnim = "normal";
+		if (hp < 0.2 && animData.exists("losing"))
+			queueAnim = "losing";
+		else if (hp > 0.8 && animData.exists("winning"))
+			queueAnim = "winning";
+
+		if (curAnim != queueAnim)
+			playAnim(queueAnim);
+
 		return health = value;
 	}
 }
